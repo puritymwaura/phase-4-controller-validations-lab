bfb398aad52af986ff8369454fe5033adcee7db6
puritymwaura <purity.mwaura@student.moringaschool.com>
2022-11-28 12:22:17 +0300

    done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
   def show
     author = Author.find(params[:id])
@@ -7,15 +9,24 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
   
   def author_params
     params.permit(:email, :name)
+  end
+
+  def render_not_found_response
+      render json: { error: "Author not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid) 
+    render json: { errors: invalid.errors.full_messages }, status: :unprocessable_entity
   end
   
 end
